b32e7119cab2a27dcd6fd9a87056116162d648ea
laurajank2 <67391108+laurajank2@users.noreply.github.com>
2022-06-21 14:24:46 -0700

    401 error
    - twitter[4707:24759] Request failed: unauthorized (401)
    - did add to timeline view controller though

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -10,6 +10,9 @@
 #import "APIManager.h"
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "TweetCell.h"
+#import "Tweet.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface TimelineViewController ()
 @property (nonatomic, strong) NSMutableArray *arrayOfTweets;
@@ -21,14 +24,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     // Get timeline
     [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
         if (tweets) {
             NSLog(@"😎😎😎 Successfully loaded home timeline");
-            for (NSDictionary *dictionary in tweets) {
-                NSString *text = dictionary[@"text"];
-                NSLog(@"%@", text);
-            }
+//            for (NSDictionary *dictionary in tweets) {
+//                NSString *text = dictionary[@"text"];
+//                NSLog(@"%@", text);
+//            }
             self.arrayOfTweets = (NSMutableArray*) tweets;
             
         } else {
@@ -41,6 +45,27 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TweetCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetCell"];
+    Tweet *tweet = self.arrayOfTweets[indexPath.row];
+    cell.author.text = tweet.user.name;
+    cell.date.text = tweet.createdAtString;
+    cell.userName.text = tweet.user.screenName;
+    cell.postContent.text = tweet.text;
+    NSString *URLString = tweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    cell.userPhoto.image = nil;
+    [cell.userPhoto setImageWithURL:urlData];
+    //cell.textLabel.text = movie[@"title"];
+    
+    return cell;
 }
 
 /*
